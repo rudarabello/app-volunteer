819b14d67bf63311bbeeb35b920310e9fbc4c873
ruda <ruda.rabello.silva@gmail.com>
2022-10-10 17:57:32 -0300

    feat: add favicon and helmet

---
+++ src/components/Favicon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Favicon from 'react-favicon'
+
+
+export default function Fav() {
+    return (
+        <>
+            <Favicon url='https://www.svgrepo.com/show/270104/loving-charity.svg' />
+        </>
+    );
+};
