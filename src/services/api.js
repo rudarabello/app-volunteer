78a687ca45aa030fe25fe7ed89235da5be55f794
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-04 20:34:46 -0300

    feat: add localhost to option for test

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://ec2-3-93-3-19.compute-1.amazonaws.com/',
 });
+
+export const localhost = axios.create({
+  baseURL: 'http://localhost:5000/',
+});
