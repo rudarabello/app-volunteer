ed414f0689b1023ddbcbfe8953fd5e72b483b7c5
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-10 15:52:47 -0300

    feat: add public routes file

---
+++ src/routes/PublicRoutes.js
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import InitialScreen from './pages/InitialScreen';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp.js';
+
+export const PublicRoutes = () => (
+    <Routes>
+        <Route path="/" element={<InitialScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+);
