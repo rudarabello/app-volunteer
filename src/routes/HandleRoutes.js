825fd62ff484216962d45dbff892aa59a0a907b3
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-10 15:51:48 -0300

    feat: add handle routes

---
+++ src/routes/HandleRoutes.js
@@ -0,0 +1,8 @@
+import { useAuth } from '../providers/AuthProvider';
+import { PrivateRoutes } from './PrivateRoutes';
+import { PublicRoutes } from './PublicRoutes';
+
+export const HandleRoute = () => {
+    const { signed } = useAuth();
+    return signed ? <PrivateRoutes /> : <PublicRoutes />;
+};
