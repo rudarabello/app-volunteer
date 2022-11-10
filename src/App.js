0221cb43df2bb8055c5e567ff4f48fbb69aaf82a
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-10 15:53:12 -0300

    fix: change architeture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Fav from "./components/Favicon";
-import Hel from "./components/Helmet";
-import Context from "./contexts/Context";
-import InitialScreen from "./routes/InitialScreen";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import SignUp from "./routes/SignUp.js";
-
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Fav from './components/Favicon';
+import Hel from './components/Helmet';
+import Context from './contexts/Context';
 
 export default function App() {
     const [data, setData] = useState([{}]);
@@ -15,14 +10,7 @@ export default function App() {
         <Context.Provider value={{ data, setData }}>
             <Fav />
             <Hel />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<InitialScreen />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/sign-up" element={<SignUp />} />                    
-                </Routes>
-            </BrowserRouter>
+            <BrowserRouter></BrowserRouter>
         </Context.Provider>
-    )
+    );
 }
